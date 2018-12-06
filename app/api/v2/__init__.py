835ff26814394eb1f6bfd078277b8f6e55d4bdf0
kirega <joseph.mutiga@gmail.com>
2018-12-06 11:26:17 +0300

    [finishes #162471488] Refresh token endpoint is complete

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from .views import SignUpEndpoint, LoginEndpoint
+from .views import (SignUpEndpoint, LoginEndpoint,
+                    RefreshTokenEndpoint)
 
 v2 = Blueprint('api', __name__, url_prefix='/api/v2')
 api = Api(v2)
 
 api.add_resource(SignUpEndpoint,'/signup')
 api.add_resource(LoginEndpoint,'/login')
+api.add_resource(RefreshTokenEndpoint,'/token')
