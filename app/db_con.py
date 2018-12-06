efaa8998f88a6ca3d8a359d14c0c518503c92d46
kirega <joseph.mutiga@gmail.com>
2018-12-06 03:32:35 +0300

    [starts #162411033] Design tables to include FK and UNQ

--- app/db_con.py
+++ app/db_con.py
@@ -11,29 +11,30 @@ URL = "dbname='{}' host='{}' port='{}' user='{}' \
  password='{}'".format(DB_NAME,DB_HOST,DB_PORT,DB_USERNAME,DB_PASS)
 
 def tables():
-    table1 = """CREATE TABLE IF NOT EXISTS "User" 
+    table1 = """CREATE TABLE IF NOT EXISTS "User"
     (   id serial PRIMARY KEY,
         first_name varchar(50) NOT NULL,
         last_name  varchar(50) NOT NULL,
         other_names varchar(50) ,
         phonenumber varchar(50) NOT NULL, 
-        username varchar(64) NOT NULL,
-        email varchar(120) NOT NULL,
-        password varchar(128) NOT NULL,
+        username varchar(64) UNIQUE,
+        email varchar(120) UNIQUE,
+        password varchar(256) NOT NULL,
         isAdmin boolean DEFAULT FALSE,
         registeredOn timestamp DEFAULT now()
     );"""
 
-    table2 = """CREATE TABLE IF NOT EXISTS "Incident" 
+    table2 = """CREATE TABLE IF NOT EXISTS "Incident"
     (   id serial PRIMARY KEY,
-        user INTEGER REFERENCES User(id) ON DELETE CASCADE,
+        user_id INTEGER,
         comment varchar(255) NOT NULL,
         incidentType varchar(25) check(incidentType in ('red-flag', 'intervention')),
         location varchar(50),
         status varchar(50) DEFAULT 'draft' check(status in ('draft','under-investigation','resolved','rejected')), 
         images bytea,
         videos bytea,
-        createdOn timestamp DEFAULT now()
+        createdOn timestamp DEFAULT now(),
+        FOREIGN KEY (user_id) REFERENCES public."User" (id)
     );"""
     table_queries = [table1,table2]
     return table_queries
