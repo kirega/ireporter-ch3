297126a94611afbbd8603aa22720acf7e0af9a49
kirega <joseph.mutiga@gmail.com>
2018-12-06 16:18:32 +0300

    [starts ##162471639] Add configs for the test

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ for creating the app
 from flask import Flask
 from flask_restful import Api
 from instance import config
+from flask_jwt_extended import JWTManager
 # Local imports
 from .api.v2 import v2
 from .errors import errors
 
 
-def create_app():
+def create_app(config):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(config.DevelopmentConfig)
+    jwt = JWTManager(app)
+    app.config.from_object(config)
     app.register_blueprint(v2)
     app.register_blueprint(errors)
     return app
--- instance/config.py
+++ instance/config.py
@@ -1,15 +1,28 @@
+import os
+
+
 class Config():
     DEBUG = False
     TESTING = False
-    
+    JWT_SECRET_KEY = 'jwt-secret-string'
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_USERNAME = os.environ.get('DB_USERNAME')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_PORT = os.environ.get('DB_PORT')
+
+
 class DevelopmentConfig(Config):
     DEBUG = True
     ENV = "development"
 
+
 class TestingConfig(Config):
     TESTING = True
+    DB_NAME = os.environ.get('DB_NAME_TEST')
+
 
 settings = {
     'development': DevelopmentConfig,
-    'testing':TestingConfig
+    'testing': TestingConfig
 }
