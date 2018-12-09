0f7fd524027185473e9889fa6fdb82a0883ed3c3
kirega <joseph.mutiga@gmail.com>
2018-12-10 01:51:42 +0300

    [starts #162411209] Add the admin status endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from .views import (SignUpEndpoint, LoginEndpoint,
                     RefreshTokenEndpoint,AllIncidentsEndpoint,
                     IncidentEndpoint, IncidentEditCommentEndpoint,
-                    IncidentEditLocationEndpoint)
+                    IncidentEditLocationEndpoint, AdminStatusEndpoint)
 
 v2 = Blueprint('api', __name__, url_prefix='/api/v2')
 api = Api(v2)
@@ -14,4 +14,5 @@ api.add_resource(AllIncidentsEndpoint,'/incidents')
 api.add_resource(IncidentEndpoint,'/incident/<int:incidentId>')
 api.add_resource(IncidentEditCommentEndpoint,'/incident/<int:incidentId>/comment')
 api.add_resource(IncidentEditLocationEndpoint,'/incident/<int:incidentId>/location')
+api.add_resource(AdminStatusEndpoint,'/incident/<int:incidentId>/status')
 api.add_resource(RefreshTokenEndpoint,'/token')
